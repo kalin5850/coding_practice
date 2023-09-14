49401d57894538c104b7a25bc20013174dd942a0
KaiLin Chang <kalin5850@gmail.com>
2023-09-14 19:51:56 -0400

    add value parentheses

--- 20_valid_parentheses.py
+++ 20_valid_parentheses.py
@@ -1,26 +1,33 @@
 class Solution:
     def is_valid1(self, s: str) -> bool:
-        length = len(s)
-        for i in range(length):
-            for j in range(length):
-                if s[i] == "(" and s[j] == ")":
-                    break
-                elif s[i] == ")" and s[j] == "(":
-                    break
-                elif s[i] == "[" and s[j] == "]":
-                    break
-                elif s[i] == "]" and s[j] == "[":
-                    break
-                elif s[i] == "{" and s[j] == "}":
-                    break
-                elif s[i] == "}" and s[j] == "{":
-                    break
-                else:
-                    return False
+        count_open_par = 0
+        count_close_par = 0
+        count_open_sq = 0
+        count_close_sq = 0
+        count_open_cur = 0
+        count_close_cur = 0
 
-        return True
+        for ch in s:
+            if ch == "(":
+                count_open_par += 1
+            elif ch == ")":
+                count_close_par += 1
+            elif ch == "[":
+                count_open_sq += 1
+            elif ch == "]":
+                count_close_sq += 1
+            elif ch == "{":
+                count_open_cur += 1
+            elif ch == "}":
+                count_close_cur += 1
+
+        return (
+            count_open_par == count_close_par
+            and count_open_sq == count_close_sq
+            and count_open_cur == count_close_cur
+        )
 
 
 if __name__ == "__main__":
-    input = "()[]"
+    input = "()["
     print(Solution().is_valid1(s=input))
