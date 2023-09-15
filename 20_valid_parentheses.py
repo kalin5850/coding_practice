0ec219f75e0d341f32263db3e264d951ba936c6b
KaiLin Chang <kalin5850@gmail.com>
2023-09-14 20:12:25 -0400

    add valid parentheses

--- 20_valid_parentheses.py
+++ 20_valid_parentheses.py
@@ -27,7 +27,21 @@ class Solution:
             and count_open_cur == count_close_cur
         )
 
+    def is_valid2(self, s: str) -> bool:
+        "use stack data structure"
+        parentheses_stack = []
+        for ch in s:
+            if ch == "(" or ch == "[" or ch == "{":
+                parentheses_stack.append(ch)
+            if ch == ")" and parentheses_stack[-1] == "(":
+                parentheses_stack.pop()
+            if ch == "]" and parentheses_stack[-1] == "[":
+                parentheses_stack.pop()
+            if ch == "}" and parentheses_stack[-1] == "{":
+                parentheses_stack.pop()
+        return len(parentheses_stack) == 0
+
 
 if __name__ == "__main__":
-    input = "()["
-    print(Solution().is_valid1(s=input))
+    input = "()[]"
+    print(Solution().is_valid2(s=input))
