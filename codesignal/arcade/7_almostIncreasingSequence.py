246d615738b488008b601018059af6c79e1e0868
KaiLin Chang <kalin5850@gmail.com>
2024-02-15 20:47:22 -0500

    update

---
+++ codesignal/arcade/7_almostIncreasingSequence.py
@@ -0,0 +1,37 @@
+"""
+Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence by removing no more than one element from the array.
+
+Note: sequence a0, a1, ..., an is considered to be a strictly increasing if a0 < a1 < ... < an. Sequence containing only one element is also considered to be strictly increasing.
+
+Example
+
+For sequence = [1, 3, 2, 1], the output should be
+solution(sequence) = false.
+
+There is no one element in this array that can be removed in order to get a strictly increasing sequence.
+
+For sequence = [1, 3, 2], the output should be
+solution(sequence) = true.
+
+You can remove 3 from the array to get the strictly increasing sequence [1, 2]. Alternately, you can remove 2 to get the strictly increasing sequence [1, 3].
+"""
+
+
+def solution(sequence):
+    removed_one = False
+    prev_maxval = None
+    maxval = None
+    for s in sequence:
+        if not maxval or s > maxval:
+            prev_maxval = maxval
+            maxval = s
+        elif not prev_maxval or s > prev_maxval:
+            if removed_one:
+                return False
+            removed_one = True
+            maxval = s
+        else:
+            if removed_one:
+                return False
+            removed_one = True
+    return True
