e5343f8543f0b4e5bb3d399e065ba68b6a59ed46
KaiLin Chang <kalin5850@gmail.com>
2024-03-03 10:54:12 -0500

    add All_Longest_String

---
+++ codesignal/arcade/9_All_Longest_Strings.py
@@ -0,0 +1,36 @@
+"""
+Given an array of strings, return another array containing all of its longest strings.
+
+Example
+
+For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
+solution(inputArray) = ["aba", "vcd", "aba"].
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[memory limit] 1 GB
+
+[input] array.string inputArray
+
+A non-empty array.
+
+Guaranteed constraints:
+1 ≤ inputArray.length ≤ 10,
+1 ≤ inputArray[i].length ≤ 10.
+
+[output] array.string
+
+Array of the longest strings, stored in the same order as in the inputArray.
+"""
+
+
+def solution(inputArray):
+    """
+    Brute force: O(n * m)
+    BCR: O(n)
+    """
+    max_length = max(len(word) for word in inputArray)
+    result = [word for word in inputArray if len(word) == max_length]
+    return result
