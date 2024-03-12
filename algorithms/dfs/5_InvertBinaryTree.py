8db9a1412ee355521413670e10020b7a7a6fb29a
KaiLin Chang <kalin5850@gmail.com>
2024-03-12 14:25:06 -0400

    add Invert Binary Tree

---
+++ algorithms/dfs/5_InvertBinaryTree.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def invert_binary_tree(tree: Node) -> Node:
+    # WRITE YOUR BRILLIANT CODE HERE
+    def pre_order(node: Node) -> Node:
+        if node == None:
+            return
+
+        node.left, node.right = node.right, node.left
+
+        pre_order(node.left)
+        pre_order(node.right)
+
+        return node
+
+    return pre_order(tree)
