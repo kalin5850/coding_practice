883ecd1fa09110886c5097f6cd21bd1b016fe188
KaiLin Chang <kalin5850@gmail.com>
2024-03-18 15:48:56 -0400

    update backtracking template

--- algorithms/backtracking/0_template.py
+++ algorithms/backtracking/0_template.py
@@ -1,3 +1,4 @@
+#  basic template
 def dfs(start_idx, path):
     if is_leaf(start_idx):
         report(path)
@@ -10,7 +11,8 @@ def dfs(start_idx, path):
     return report
 
 
-def dfs_v_1_1(start_idx, path):
+# prune the branches
+def dfs_v_1(start_idx, path):
     if is_leaf(start_idx):
         report(path)
         return
@@ -24,3 +26,22 @@ def dfs_v_1_1(start_idx, path):
         path.pop()
 
     return report
+
+
+# prune the branches and check state
+def dfs_v_2(start_idx, path, [... additional_states]):
+    if is_leaf(start_idx):
+        report(path)
+        return
+    
+    for edge in get_edge(start_idx, [... additional_states]):
+        # prune if needed
+        if not is_valid(edge):
+            continue
+        path.add(edge)
+        if additional_states:
+            update(... additional_states)
+        dfs(start_idx + len(edge), path, [... additional_states])
+        path.pop()
+        
+    return
