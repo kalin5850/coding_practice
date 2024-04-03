586428fd1e7f5800f408b958259307345ec6e350
KaiLin Chang <kalin5850@gmail.com>
2024-04-03 13:32:45 -0400

    add dynamic programming

--- algorithms/8_dynamic_programming/1_NumberofUniquePathstoGofromTopLefttoBottomRight.py
+++ algorithms/8_dynamic_programming/1_NumberofUniquePathstoGofromTopLefttoBottomRight.py
@@ -29,9 +29,24 @@ Output: 15
 """
 
 
+#  dynamic programming
+def unique_paths(m: int, n: int) -> int:
+    dp = [[0 for _ in range(n)] for _ in range(m)]
+
+    # base
+    for r in range(m):
+        dp[r][0] = 1
+    for c in range(n):
+        dp[0][c] = 1
+    for r in range(1, m):
+        for c in range(1, n):
+            dp[r][c] = dp[r - 1][c] + dp[r][c - 1]
+
+    return dp[-1][-1]
+
+
 #  dfs and cached
 def unique_paths(m: int, n: int) -> int:
-    # WRITE YOUR BRILLIANT CODE HERE
 
     def dfs(node, count, cached):
         row, col = node
