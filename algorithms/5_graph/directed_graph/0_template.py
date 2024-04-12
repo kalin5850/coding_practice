f8bb1d15e043af9d198b35b826caae587c3eb0cc
KaiLin Chang <kalin5850@gmail.com>
2024-04-12 11:48:07 -0400

    add topological sort

---
+++ algorithms/5_graph/directed_graph/0_template.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+"""
+    graph = {
+        a: [...],
+        b: [...],
+        c: [...]
+    }
+"""
+
+
+def find_degree(graph):
+    indegree = {node: 0 for node in graph}
+    for node in graph:
+        for neighbor in graph[node]:
+            indegree[neighbor] += 1
+    return indegree
+
+
+def topo_sort(graph):
+    result = []
+    queue = deque()
+    indegree = find_degree(graph)
+    for node in indegree:
+        if indegree[node] == 0:
+            queue.append(node)
+    while len(queue):
+        curr = queue.popleft()
+        result.append(curr)
+        for neighbor in graph[curr]:
+            indegree[neighbor] -= 1
+            if indegree[neighbor] == 0:
+                queue.append(neighbor)
+    return result if len(graph) == len(result) else None
