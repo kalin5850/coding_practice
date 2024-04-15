3f677e50c72f1574f6267432dbf255b9c486f3c6
KaiLin Chang <kalin5850@gmail.com>
2024-04-15 14:50:19 -0400

    update

--- algorithms/6_two_pointers/9_LongestSubstringwithoutRepeatingCharacters.py
+++ algorithms/6_two_pointers/9_LongestSubstringwithoutRepeatingCharacters.py
@@ -1,21 +1,20 @@
 def longest_substring_without_repeating_characters(s: str) -> int:
+    longest_length = 0
     slow = 0
-    max_length = 0
     for fast in range(1, len(s)):
         if s[fast] in s[slow:fast]:
-            max_length = max(max_length, len(s[slow:fast]))
-            while True:
-                if s[slow] == s[fast]:
-                    slow += 1
-                    break
+            longest_length = max(longest_length, len(s[slow:fast]))
+            while s[slow] != s[fast]:
                 slow += 1
+            slow += 1
         if fast == len(s) - 1:
-            max_length = max(max_length, len(s[slow : fast + 1]))
+            longest_length = max(longest_length, len(s[slow : fast + 1]))
 
-    return max_length
+    return longest_length
 
 
 if __name__ == "__main__":
+    s = "abccabcabcc"
     s = "abcdbea"
     res = longest_substring_without_repeating_characters(s)
     print(res)
