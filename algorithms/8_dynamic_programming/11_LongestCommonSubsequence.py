5b56454f621fa731ce68feae8fbb0548620ee959
KaiLin Chang <kalin5850@gmail.com>
2024-05-01 15:20:29 -0400

    add dfs

---
+++ algorithms/8_dynamic_programming/11_LongestCommonSubsequence.py
@@ -0,0 +1,32 @@
+def longest_common_subsequence(word1: str, word2: str) -> int:
+
+    def dfs(idx1: int, idx2: int, length: int) -> int:
+        if idx1 >= len(word1) or idx2 >= len(word2):
+            return 0
+        if word1[idx1] == word2[idx2]:
+            length = dfs(idx1 + 1, idx2 + 1, length) + 1
+        else:
+            length = max(dfs(idx1 + 1, idx2, length), dfs(idx1, idx2 + 1, length))
+        return length
+
+    print(dfs(0, 0, 0))
+    return 0
+
+
+if __name__ == "__main__":
+    word1 = "abcde"
+    word2 = "ace"
+    res = longest_common_subsequence(word1, word2)
+    print(res)
+    word1 = "almost"
+    word2 = "algomonster"
+    res = longest_common_subsequence(word1, word2)
+    print(res)
+    word1 = "abcd"
+    word2 = "hjkl"
+    res = longest_common_subsequence(word1, word2)
+    print(res)
+    word1 = "inspire"
+    word2 = "aspire"
+    res = longest_common_subsequence(word1, word2)
+    print(res)
