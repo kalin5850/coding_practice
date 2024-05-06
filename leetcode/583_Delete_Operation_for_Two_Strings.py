09c49200183163859da18d11502181c36e4f82da
KaiLin Chang <kalin5850@gmail.com>
2024-05-06 16:33:53 -0400

    add dynamic programming with top to down

--- leetcode/583_Delete_Operation_for_Two_Strings.py
+++ leetcode/583_Delete_Operation_for_Two_Strings.py
@@ -26,6 +26,20 @@ word1 and word2 consist of only lowercase English letters.
 def min_distance(w1: str, w2: str) -> int:
     m, n = len(w1), len(w2)
 
+    def dynamic_programming():
+        dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+        for i in range(m + 1):
+            dp[0][i] = i
+        for j in range(n + 1):
+            dp[j][0] = j
+        for r in range(1, n + 1):
+            for c in range(1, m + 1):
+                if w1[c - 1] == w2[r - 1]:
+                    dp[r][c] = dp[r - 1][c - 1]
+                else:
+                    dp[r][c] = 1 + min(dp[r - 1][c], dp[r][c - 1])
+        return dp[-1][-1]
+
     def dfs_cached(idxi: int, idxj: int, total: int, cached: dict[int, int]) -> int:
         if (idxi, idxj) in cached:
             return cached.get((idxi, idxj), 0)
@@ -56,6 +70,7 @@ def min_distance(w1: str, w2: str) -> int:
 
     print(dfs(0, 0, 0))
     print(dfs_cached(0, 0, 0, {}))
+    print(dynamic_programming())
 
     return ""
 
